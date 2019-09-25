3386ad3f7fdbee56e53707309455f1ae98db7598
Goteer <joaquindiazchurria@gmail.com>
2019-09-25 15:05:41 -0300

    Cambio de implementacion, conexion funciona

--- esp8266_a_influxDB.ino
+++ esp8266_a_influxDB.ino
@@ -1,17 +1,18 @@
 #include <ESP8266WiFi.h>
 #include <InfluxDb.h>
 
- #define INFLUXDB_HOST "163.10.10.123" //Si no funciona, verificar firewall y puertos. Suele funcionar con ip publica.
- #define INFLUXDB_PORT 9999
- #define INFLUXDB_DATABASE "prueba" //Nombre de la base de datos
- #define INFLUXDB_ORG "prueba" //nombre organizacion
- #define INFLUXDB_BUCKET "cubeta" //nombre bucket que recibe datos
+ #define INFLUXDB_HOST "influxdb-c.catedras.linti.unlp.edu.ar" //Si no funciona, verificar firewall y puertos. Suele funcionar con ip publica.
+ #define INFLUXDB_PORT 443
+ #define INFLUXDB_DATABASE "joaquindiazchurria" //Nombre de la base de datos
+ //#define INFLUXDB_ORG "joaquindiazchurria" //nombre organizacion
+ //#define INFLUXDB_BUCKET "cubeta" //nombre bucket que recibe datos
 
- #define INFLUXDB_TOKEN "VGJcpLkEZUTAtZW5nG3uS1s2LOfV8slUR6m6L1UY0qTz9ab7qc0mKxG9spKVk72gSn4iUWVCQyc-drRfpUdSsA==" //Token de acceso para autorizar al dispositivo
+ //#define INFLUXDB_TOKEN "n/a" //Token de acceso para autorizar al dispositivo
  
  //Si resulta necesario autenticarse...
-// #define INFLUXDB_USER "user"//Datos del usuario del dispositivo
-// #define INFLUXDB_PASS "pass"
+ #define INFLUXDB_USER "joaquindiazchurria"//Datos del usuario del dispositivo
+ 
+ #define INFLUXDB_PASS "joaquindiazchurria_iot"
 
  #define DISPOSITIVO "d1" //Nombre del dispositivo
  #define SENSOR_OBSTRUCCION "reflectivo"
@@ -24,8 +25,10 @@ const byte INTERRUPT_PIN_SENSOR_INT = 13; //Corresponde al pin D7 en la placa ES
 const byte INTERRUPT_PIN_SENSOR_EXT = 12; //Corresponde al pin D6 en la placa. Sensor exterior.
 volatile byte interruptCounter_int = 0; //Cantidad de interrupciones pendientes
 volatile byte interruptCounter_ext = 0;
-const int SENSOR_DELAY = 40; //Con 40, funciona correctamente, aunque los sensores tienen que estar a 3 dedos de distancia entre si.
+const int SENSOR_DELAY = 100; //Con 40, funciona correctamente, aunque los sensores tienen que estar a 3 dedos de distancia entre si.
 
+unsigned int tiempo_int = 0;
+unsigned int tiempo_ext = 0;
 
 // -------------------------------- SETUP ------------------------------
 
@@ -48,11 +51,11 @@ void setup() {
 
   
  // connect to WiFi
-  const char* ssid     = "ox-demo";
-  const char* password = "ox-over-dns";
+  const char* ssid     = "ox-demo"; //"Linti";
+  const char* password = "ox-over-dns";//"passdelap2015!";
   
-
-
+//usuario de grafana
+//usuario_iot
 
   delay(10);
 
@@ -84,16 +87,21 @@ void setup() {
  Serial.println("Conectando a influx...");
 
  // set the target database
- influx.setDb(INFLUXDB_DATABASE);
+ //influx.setDb(INFLUXDB_DATABASE);
+
+ // or use a db with auth
+  influx.setDbAuth(INFLUXDB_DATABASE, INFLUXDB_USER, INFLUXDB_PASS); // with authentication
+
  
- influx.setVersion(2);
- influx.setOrg(INFLUXDB_ORG);
- influx.setBucket(INFLUXDB_BUCKET);
- influx.setToken(INFLUXDB_TOKEN);
- influx.setPort(INFLUXDB_PORT);
+// influx.setVersion(1);
+// influx.setOrg(INFLUXDB_ORG);
+// influx.setBucket(INFLUXDB_BUCKET);
+// influx.setToken(INFLUXDB_TOKEN);
+// influx.setPort(INFLUXDB_PORT);
  valorSimulado = 0;
 
  Serial.println("Conectado a influx!");
+
 
 }
 
@@ -198,76 +206,64 @@ void influx_send_test_value(double value){
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  if (interruptCounter_int>0) { //Gestor de interrupcion cuando se detecta una obstruccion desde adentro
+
+  if (interruptCounter_int>0) {
+    tiempo_int+=1;
+    }
+
+  if (interruptCounter_ext>0) {
+    tiempo_ext+=1;
+    }
+
+  if (tiempo_int>900000){
+     //Si estuvo mucho tiempo esperando el otro sensor, reiniciar contadores
+            interruptCounter_int=0;
+            interruptCounter_ext=0;
+            tiempo_ext=0;
+            tiempo_int=0;
+          Serial.println("Reinicio int");
+          
+    }
     
 
-    delay(SENSOR_DELAY); //Revisar si la obstruccion dura al menos 300ms para medio-confirmar que es una persona
-    sensorIntEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_INT);
-    if (sensorIntEstaObstruido) {
-      delay(SENSOR_DELAY); 
-      sensorIntEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_INT);
-      sensorExtEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_EXT);
-      if (sensorExtEstaObstruido or sensorIntEstaObstruido) {
-        delay(SENSOR_DELAY); 
-        sensorIntEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_INT);
-        sensorExtEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_EXT);
-        //Si el sensor estuvo obstruido por al menos 150ms, se envia un paquete informando que pasó una pesona.
-        if (sensorExtEstaObstruido) {
-          Serial.print("Salida reconocida, avisando a la base de datos que salio una persona: "); Serial.println(valorSimulado);
-          influx_send_sale_persona();
-          //influx_test_limpieza();
-          sensorExtEstaObstruido=false;
-          }
+
+    if (tiempo_ext>900000){
+     //Si estuvo mucho tiempo esperando el otro sensor, reiniciar contadores
+            interruptCounter_int=0;
+            interruptCounter_ext=0;
+            tiempo_ext=0;
+            tiempo_int=0;
+          Serial.println("Reinicio ext");
+          
+    }
+
+    if (interruptCounter_int>10) { //Si se tapo el interior
+      if (interruptCounter_ext>10 && interruptCounter_ext<interruptCounter_int && tiempo_ext<1000) { //Revisar si se tapo el exterior *despues* del interior
+        Serial.println("Persona sale");
+        influx_send_sale_persona();
+        interruptCounter_int=0;
+        interruptCounter_ext=0;
+        tiempo_int+=800000;
+        tiempo_ext+=800000;
         }
-      }else{
-        Serial.println("Interno activado por error");
-        }
+      }
+
+      
+      
+      if (interruptCounter_ext>10) {
+      if (interruptCounter_int>10 && interruptCounter_int<interruptCounter_ext && tiempo_int<1000) {//Revisar si se tapo el interior *despues* del exterior
+        Serial.println("Persona entra");
+        influx_send_entra_persona();
+        interruptCounter_int=0;
+        interruptCounter_ext=0;
+        tiempo_ext+=800000;
+        tiempo_int+=800000;
+          
+      }
+      
+    }
 
     
-    
-     
-    sensorIntEstaObstruido=false;
-    interruptCounter_ext=0;
-    interruptCounter_int=0; //Para inhibir la interrupcion disparada por el otro sensor
-    } //Fin interrupcion.
-
-    
-    if (interruptCounter_ext>0) { //Gestor de interrupcion cuando se detecta una obstruccion desde adentro
-    
-
-    delay(SENSOR_DELAY); //Revisar si la obstruccion dura al menos 300ms para medio-confirmar que es una persona
-    sensorExtEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_EXT);
-    if (sensorExtEstaObstruido) {
-      delay(SENSOR_DELAY); 
-      sensorIntEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_INT);
-      sensorExtEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_EXT);
-      if (sensorExtEstaObstruido or sensorIntEstaObstruido) {
-        delay(SENSOR_DELAY); 
-        sensorIntEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_INT);
-        sensorExtEstaObstruido=!digitalRead(INTERRUPT_PIN_SENSOR_EXT);
-        //Si el sensor estuvo obstruido por al menos 150ms, se envia un paquete informando que pasó una pesona.
-        if (sensorIntEstaObstruido) {
-          Serial.print("Entrada reconocida, avisando a la base de datos que entro una persona: "); Serial.println(valorSimulado);
-          Serial.print("Valor int:"); Serial.print(sensorIntEstaObstruido); Serial.print("Valor ext:"); Serial.println(sensorExtEstaObstruido);
-          influx_send_entra_persona();
-          //influx_test_limpieza();
-          sensorIntEstaObstruido=false;
-          }
-        }
-      } else{
-        Serial.println("Externo activado por error");
-        }
-  
-    
-    
-     
-    sensorExtEstaObstruido=false;
-    interruptCounter_ext=0;
-    interruptCounter_int=0; //Para inhibir la interrupcion disparada por el otro sensor
-    } //Fin interrupcion.
-
-
-    
+   // Serial.print("interno counter: "); Serial.print(interruptCounter_int); Serial.print("externo counter: "); Serial.println(interruptCounter_ext);
     
 }
